2a57024e339f394dc3f092e5b6414e9628654631
nelipau17 <ponchik0108@icloud.com>
2024-07-19 12:59:46 +0500

    Initial Commit

---
+++ termeet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  termeet
+//
+//  Created by Polina Popova on 19/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
