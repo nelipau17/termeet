fedfdfda05803f00be84092a217bdb0afaf882e6
nelipau17 <ponchik0108@icloud.com>
2024-08-04 10:00:41 +0500

    network

---
+++ termeet/Network/Encoding/JSONParameterEncoder.swift
@@ -0,0 +1,10 @@
+//
+//  JSONParameterEncoder.swift
+//  termeet
+//
+//  Created by Polina Popova on 03/08/2024.
+//
+
+import Foundation
+
+
--- termeet/Network/Encoding/URLParameterEncoder.swift
+++ termeet/Network/Encoding/URLParameterEncoder.swift
@@ -7,8 +7,8 @@
 
 import Foundation
 
-public static URLParameterEncoder: ParameterEncoder{
-    public static func encoder(urlReqest: inout URLRequest, with parameters: Parameters) throws {
+public struct URLParameterEncoder: ParameterEncoder{
+    public static func encode(urlRequest: inout URLRequest, with parameters: Parameters) throws {
         guard let url = urlRequest.url else { throw NetworkError.missingURL }
         
         if var urlComponents  = URLComponents(url: url, resolvingAgainstBaseURL: false), !parameters.isEmpty {
@@ -18,10 +18,10 @@ public static URLParameterEncoder: ParameterEncoder{
                 let queryItem = URLQueryItem(name: key, value: "\(value)".addingPercentEncoding(withAllowedCharacters: .urlHostAllowed))
                 urlComponents.queryItems?.append(queryItem)
             }
-            urlReqest.url= urlComponents.url
+            urlRequest.url = urlComponents.url
         }
-        if urlReqest.value(forHTTPHeaderField: "Content-Type") == nil {
-            
+        if urlRequest.value(forHTTPHeaderField: "Content-Type") == nil {
+            urlRequest.setValue("application/x-www-form-urlencoded; charset=utf-8", forHTTPHeaderField: "Content-Type")
         }
     }
 }
